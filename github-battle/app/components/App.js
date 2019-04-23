328994793b8f00b730d3ae04d7acadb0d02e2c32
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-23 17:45:02 -0500

    Create skeleton of Battle component. Create 404 page using React Router' switch.

--- github-battle/app/components/App.js
+++ github-battle/app/components/App.js
@@ -4,10 +4,13 @@ var Popular = require('./Popular');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 
 var Nav = require('./Nav');
 var Home = require('./Home');
+var Battle = require('./Battle');
 
+// in the Switch component, if all other routes aren't active, the page path isn't a page we serve up. Instead we render 'Not Found'
 class App extends React.Component {
 		/* render Router, set components to be rendered only at specific paths */
 	render() {
@@ -15,10 +18,14 @@ class App extends React.Component {
 			<Router>
 			<div className="container">
 				<Nav />
-
-				<Route exact path='/' component={Home} />
-				<Route path='/popular' component={Popular} />
-
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route path='/popular' component={Popular} />
+					<Route path='/battle' component={Battle} />
+					<Route render={function() {
+						return <p>Not Found</p>
+					}} />
+				</Switch>
 			</div>
 			</Router>
 		)
--- github-battle/webpack.config.js
+++ github-battle/webpack.config.js
@@ -8,17 +8,23 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 // css files are transformed with the style-loader and css-loader
 
 // html-webpack-plugin automatically generates an index.html file that dynamically includes a reference to our output file
+
+// publicPath sets the base for all of our assets - long story short, this actually redirects us to '/' when we request assets for a page
 module.exports = {
 	entry: './app/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'index_bundle.js'
+		filename: 'index_bundle.js',
+		publicPath: '/'
 	},
 	module: {
 		rules: [
 			{ test: /\.(js)$/, use: 'babel-loader'},
 			{ test: /\.css$/, use: ['style-loader', 'css-loader']}
 		]
+	},
+	devServer: {
+		historyApiFallback: true
 	},
 	mode: 'development',
 	plugins: [
