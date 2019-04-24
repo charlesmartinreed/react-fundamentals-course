63afdd63b63a31774ce597f3c2daa3291f9d6ddb
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-24 18:13:17 -0500

    Create build.

--- github-battle/webpack.config.js
+++ github-battle/webpack.config.js
@@ -10,6 +10,9 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 // html-webpack-plugin automatically generates an index.html file that dynamically includes a reference to our output file
 
 // publicPath sets the base for all of our assets - long story short, this actually redirects us to '/' when we request assets for a page
+
+// changing mode to 'production' does things like set node env to production, minifies our code, etc. This is handled dynamically by calling the 'build' script in package.json
+
 module.exports = {
 	entry: './app/index.js',
 	output: {
@@ -26,7 +29,7 @@ module.exports = {
 	devServer: {
 		historyApiFallback: true
 	},
-	mode: 'development',
+	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: 'app/index.html'
