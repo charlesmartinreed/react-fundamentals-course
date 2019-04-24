28e04f387238b44b2df54a316847444c22b37267
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-24 17:55:28 -0500

    Add setInterval to componentDidMount. Add speed prop to Loading component.

--- github-battle/app/components/Loading.js
+++ github-battle/app/components/Loading.js
@@ -13,8 +13,35 @@ class Loading extends React.Component {
 		super(props)
 
 		this.state = {
-			text: props.text
+			text: props.text,
 		}
+	}
+	// when manipulating previous state, pass prevState into setState method.
+
+	componentDidMount() {
+		var stoppper = this.props.text + '...';
+
+		this.interval = window.setInterval(function() {
+			if(this.state.text === stoppper) {
+				this.setState(function() {
+					return {
+						text: this.props.text
+					}
+				})
+			} else {
+				this.setState(function(prevState) {
+					return {
+						text: prevState.text + '.'
+					}
+				})
+			}
+		}.bind(this), this.props.speed)
+	}
+
+	// remove the setInterval call
+	componentWillUnmount() {
+			console.log('interval cleared');
+			window.clearInterval(this.interval);
 	}
 
 	render() {
@@ -27,11 +54,13 @@ class Loading extends React.Component {
 }
 
 Loading.propTypes = {
-	text: PropTypes.string.isRequired
+	text: PropTypes.string.isRequired,
+	speed: PropTypes.number.isRequired
 }
 
 Loading.defaultProps = {
-	text: 'Loading'
+	text: 'Loading',
+	speed: 300
 }
 
 module.exports = Loading;
